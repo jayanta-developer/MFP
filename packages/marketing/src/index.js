43de9993257d09ac89ad0fd57c0cd01fb66bdb40
jayanta dey <jayantadey398@gmail.com>
2025-07-30 12:17:02 +0530

    implement marketing app

--- packages/marketing/src/index.js
+++ packages/marketing/src/index.js
@@ -1,1 +1,19 @@
-console.log("Marketing");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const mount = (el) => {
+  const root = ReactDOM.createRoot(el);
+  root.render(<h1>Marketing Page</h1>);
+};
+
+//for development
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_marketing_root");
+
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+//for producatioin
+export { mount };

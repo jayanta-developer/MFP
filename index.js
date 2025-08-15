953e26f9c0eed0fe0f71004bd7a981694581592a
Jayanta dey <jayantadey398@gmail.com>
2025-08-15 15:33:13 +0530

    int

---
+++ index.js
@@ -0,0 +1,25 @@
+// const string = "I am Jayanta";
+
+// const revercedString = string.split(" ").reverse();
+// let revArr = [];
+
+// revercedString.map((el) => {
+//   revArr.push(el.split("").reverse().join(""));
+// });
+
+// console.log(revArr.join(" "));
+
+// console.log("A", - 1);
+// console.log("Afsd","100");
+
+// let arr1 = [, , , ,];
+// let arr3 = [
+//   [1, 2, 3, 4],
+//   [0, 0, 0, 0, 0],
+// ];
+// console.log(arr1.length);
+// console.log(arr3.length);
+
+let arr = [21, 54, 24, 100, 23, 0, 4, 5, 76, 32];
+//only sort will compar betwin string
+console.log(arr.sort((a,b)=>a-b));
